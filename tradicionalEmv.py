411dd722de655eae55efca305f16fdd9ffbf1e19
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-09 19:12:35 -0500

    nt8 feat se crea ejemplo de funcion tradicional

---
+++ tradicionalEmv.py
@@ -0,0 +1,14 @@
+'''numeroUno = 5;
+numeroDos = 10;
+sumatoria = numeroUno + numeroDos;
+print(f"La suma es {sumatoria}");'''
+
+#Declarando una funcion tradicional
+def sumarNumeros(numeroUno, numeroDos):
+    sumatoria = numeroUno + numeroDos;
+    return sumatoria;
+
+#Llamando una función tradicional
+
+resultado = sumarNumeros(5,10);
+print(f"La suma es {resultado}");
