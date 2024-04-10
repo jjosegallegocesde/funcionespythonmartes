d95bf63a903b4d786dd0470ae2b7d1899c7f702e
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-09 19:12:34 -0500

    NT9 Feat se crea ejemplo de función tradicional

---
+++ tradicionalesm.py
@@ -0,0 +1,13 @@
+# numeroUno=5
+# numeroDos=10
+# sumatoria=numeroUno+numeroDos
+# print(f"La suma es: {sumatoria}")
+
+#Declarando una función tradicional
+def addNumbers (numberOne, numberTwo):
+    add=numberOne+numberTwo
+    return add
+
+#Llamando una función tradicional
+result=addNumbers(10,20)
+print(f"La suma es: {result}")
