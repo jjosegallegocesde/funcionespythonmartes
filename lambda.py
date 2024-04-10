b0b0264db2c2129f4918d2b43a62b94cda4fa035
yul100 <yulgc123@gmail.com>
2024-04-09 20:26:54 -0500

    nt8  feat  se crea la funcion lambda

---
+++ lambda.py
@@ -0,0 +1,6 @@
+#declarando una funcion lambda
+sumar_numeros=lambda numero1 , numero2: numero1 + numero2
+
+#llamando una funcion lambda
+resultado =sumar_numeros(10,10)
+print(f"la suma es :  {resultado}")
