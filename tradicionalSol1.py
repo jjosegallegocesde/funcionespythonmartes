ceb276eac61294f0e464f844334a02dfb90cc30b
RoosiRomero <128660384+RoosiRomero@users.noreply.github.com>
2024-04-09 19:12:45 -0500

    NT8 feat se crea ejemplo de función tradicional

---
+++ tradicionalSol1.py
@@ -0,0 +1,20 @@
+'''numeroUno=5
+numeroDos=10
+sumatoria =numeroUno+numeroDos
+print (f"la suma es {sumatoria}")
+numeroTres=5
+numeroCuatro=10
+sumatoria =numeroTres+numeroCuatro
+print (f"la suma es {sumatoria}")'''
+
+#Declarando una funcion tradicional
+def sumarNumeros(numeroUno, numeroDos):
+    sumatoria= numeroUno+numeroDos
+    return sumatoria
+
+#Llamando función tradicional
+numeroUno=5
+numeroDos=20
+numeroTres=50
+resultado=sumarNumeros(numeroUno,numeroTres)
+print (f"la suma es:{resultado}")
