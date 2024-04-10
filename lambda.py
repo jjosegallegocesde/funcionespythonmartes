abf5867552a02689260f88ad5b136f26fcef4b5d
RoosiRomero <128660384+RoosiRomero@users.noreply.github.com>
2024-04-09 20:32:36 -0500

    nt8 se crea lambda

--- lambda.py
+++ lambda.py
@@ -1,5 +1,5 @@
 #Declarando funcion lambda
-sumarNumeros=lambda numero1,numero2: numero1+numero2
+sumarNumeros=lambda numero1,numero2:numero1+numero2
 
 #Llamando una funcion lambda
 resultado=sumarNumeros(10,10)
