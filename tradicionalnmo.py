1541ce3fb621d5194cc391d2d024bed333b6d5c6
MteoVlez99 <143894994+MteoVlez99@users.noreply.github.com>
2024-04-09 19:56:57 -0500

    nt8 feat se hacen cambios

--- tradicionalnmo.py
+++ tradicionalnmo.py
@@ -1,5 +1,5 @@
 
-numeroUno=5
+""""numeroUno=5
 numeroDos=10
 sumatoria=numeroUno+numeroDos
 print(f" la suma es {sumatoria}")
@@ -7,6 +7,15 @@ print(f" la suma es {sumatoria}")
 numeroTres=3
 numeroCuatro=10
 sumatoriaDos=numeroTres+numeroCuatro
-print(f" la suma es {sumatoriaDos}")
+print(f" la suma es {sumatoriaDos}")"""
 
+def sumarNumeros(numeroUno, numeroDos):
+    sumatoria=numeroUno+numeroDos
+    return sumatoria
+
+numeroUno=5
+numeroDos=20
+nuemroTres=50
+resultado=sumarNumeros(5,20)
+print(f"la suma es:{resultado}")
 
