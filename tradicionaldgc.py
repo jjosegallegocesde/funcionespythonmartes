d09736ea73e953ac9fb827c0efc412776b474657
DuverneyGarcia <duver.gc33@gmail.com>
2024-04-09 19:12:48 -0500

    nt8 se crea ejemplo de funcion tradicional

---
+++ tradicionaldgc.py
@@ -0,0 +1,19 @@
+'''numeroUno=5
+numeroDos=10
+sumatoria=numeroUno+numeroDos
+print(f"la suma es: {sumatoria}")
+numeroTres=5
+numeroCuatro=10
+sumatoriaDos=numeroUno+numeroDos
+print(f"la suma es: {sumatoriaDos}")'''
+
+#Declarando una funcion tradicional. 
+def sumarNumeros(numeroUno,numeroDos):
+    sumatoria=numeroUno+numeroDos
+    return sumatoria
+
+#llamando una funcion tradicional.
+numeroUno=5
+numeroDos=10
+numeroTres=50
+resultado=sumarNumeros(20,15)
