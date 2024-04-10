60a4436cf57b51d87f14247164e94e3be2fc44c4
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-09 20:26:19 -0500

    nt8 feat se crea funcion lambda

---
+++ lambda.py
@@ -0,0 +1,6 @@
+#Declarando una función lambda
+sumarNumeros = lambda numero1,numero2 : numero1 + numero2;
+
+#llamando una función lambda
+resultado = sumarNumeros(10,10);
+print(f"La suma es {resultado}");
