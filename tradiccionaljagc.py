e540a1cb7040f179e377665ae388b08a61e7808b
yul100 <yulgc123@gmail.com>
2024-04-09 19:12:51 -0500

    nt8 feat se crea ejemplo de funcion tradicional

---
+++ tradiccionaljagc.py
@@ -0,0 +1,24 @@
+""" numero_uno=5
+numero_dos=10
+
+sumatoria= numero_uno + numero_dos
+
+print(f"la suma es {sumatoria}")
+ """
+
+#Declarando una funcion  tradiccional
+
+
+def sumar_numeros(numero_uno,numero_dos):
+    sumatoria=numero_uno + numero_dos
+    return sumatoria
+
+ #llamando una funcion tradiccional
+numero_uno = 5
+numero_dos = 20
+numero_tres = 50
+
+resultado = sumar_numeros(10,20)
+print(f" la suma es : {resultado}")
+
+
