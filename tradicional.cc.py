c9acf4f6c54ba273775d57cc786894e1189b8298
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-09 20:03:40 -0500

    nt8 feat se crea ejemplo de funciones

---
+++ tradicional.cc.py
@@ -0,0 +1,14 @@
+
+#Declarando funciones
+
+def sumarNumero (numeroUno, numeroDos):
+ Sumatoria = numeroUno+numeroDos
+ return Sumatoria
+
+
+#Llamando una funcion tradicional
+numeroUno=4
+numeroDos= 5
+numeroTres=10
+resultado = sumarNumero(numeroUno, numeroTres)
+print(f"La suma es: {resultado}")
