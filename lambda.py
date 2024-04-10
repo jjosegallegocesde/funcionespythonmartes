d236a3d7693f09cf0e6580d2a8dfc99009c4f10c
melanyvalle <vallemelanie717@gmail.com>
2024-04-09 20:26:58 -0500

    nt8 refactor se agrega ejemplo de funciones lambda

---
+++ lambda.py
@@ -0,0 +1,8 @@
+#Declarando una funcion lambda
+
+sumarNumeros=lambda numero1, numero2: numero1 + numero2 
+
+#llamando la funcion lambda
+
+resultado= sumarNumeros(10,10)
+print(f"la suma es {resultado}")
