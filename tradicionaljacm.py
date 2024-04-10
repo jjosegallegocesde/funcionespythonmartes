65eb1c254a3a8e7e4398d46ea193927ff6e7cb5a
Daniel Rios <danielriosherrera1516@gmail.com>
2024-04-09 19:12:05 -0500

    nt8 feat se crea ejemplo de funcion tradicional

---
+++ tradicionaljacm.py
@@ -0,0 +1,15 @@
+'''numeroUno=5
+numeroDos=10
+sumatoria = numeroUno + numeroDos
+print (f" la suma es {sumatoria}")'''
+#Declarando una funcion tradicional
+def sumarNumeros(numeroUno, numeroDos):
+    sumatoria = numeroUno + numeroDos
+    return sumatoria
+
+#Llamando una funcion tradicional
+numeroUno=5
+numeroDos=20
+numeroTres=50
+resultado = sumarNumeros(10,20)
+print (f"la suma es: {resultado}")
