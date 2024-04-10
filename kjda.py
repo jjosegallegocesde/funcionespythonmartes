bc41d93c938a8c0ac3850a06f80ccca17aaa5536
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-09 19:12:08 -0500

    nt8 feat se crea ejemplo de funcion tradicional

---
+++ kjda.py
@@ -0,0 +1,12 @@
+''''numeroUno = 5
+numeroDos = 10
+sumatoria = numeroUno + numeroDos
+print(sumatoria)'''
+
+
+def sumarNumeros(numeroUno , numeroDos):
+    sumatoria = numeroUno+numeroDos
+    return sumatoria
+
+resultado = sumarNumeros(1,2)
+print(f"la suma es: {resultado}")
