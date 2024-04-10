02cebfd0b499e60c4c41f01fd5a77587c1c88ace
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-09 20:27:00 -0500

    nt8 feat se crea ejemplo de una funcion lambda

---
+++ lambda.py
@@ -0,0 +1,8 @@
+#Declarando una funcion
+
+sumerNumero= lambda numeroUno, numeroDos:numeroUno + numeroDos
+
+#Llamar una funcion
+resultado = sumerNumero(10,10)
+
+print(f"La suma es igual: {resultado}")
