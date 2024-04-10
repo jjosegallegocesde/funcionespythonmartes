7452af76e076b5557ed0d21e24757dbac998163a
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-09 19:12:47 -0500

    nt8 se crea ejemplo de funcion tradicional

---
+++ tradicionalkyp.py
@@ -0,0 +1,16 @@
+'''numeroUno = 5
+numeroDos = 10
+sumatoria = numeroUno + numeroDos
+print(f"La suma es {sumatoria}")'''
+
+#Declarando una funcion tradicional
+def sumarNumeros(numeroUno,numeroDos):
+    sumatoria = numeroUno + numeroDos
+    return sumatoria
+
+#Llamando una funcion tradicional
+numeroUno = 5
+numeroDos = 10
+numeroTres = 20
+resultado = sumarNumeros(numeroUno,numeroTres)
+print(f"La suma es {resultado}")
