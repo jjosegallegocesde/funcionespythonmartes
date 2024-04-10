a3d33c23ddc5e0161b9cafaa0582e8ef93ab8741
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-09 19:13:03 -0500

    nt8 feat se crea ejemplo de funcion tradicional

---
+++ tradicionalwog.py
@@ -0,0 +1,29 @@
+# numero_uno = 5
+# numero_dos = 10
+# sumatoria=numero_uno + numero_dos
+
+# print(f"La suma es {sumatoria}")
+
+# numero_tres = 5
+# numero_cuatro = 10
+# sumatoria_dos=numero_tres + numero_cuatro
+
+# print(f"La suma es {sumatoria_dos}")
+
+#declarando una funcion
+
+def sumar_numeros (numero_uno, numero_dos):
+
+    sumatoria=numero_uno+numero_dos
+
+    return sumatoria
+
+#llamando una funcion tradicional
+
+numero_uno=5
+numero_dos=20
+numero_tres=50
+
+resultado= sumar_numeros (numero_uno,numero_tres)
+
+print(f"la suma es: {resultado}")
