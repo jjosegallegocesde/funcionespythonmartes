fa34379a568bb67d7a5ed96c535713870097e4fb
Segura <ricardo.segura@softwareone.com>
2024-04-09 19:12:07 -0500

    nt8 feat se crea ejemplo de funcion tradicional

---
+++ tradicionalrasm.py
@@ -0,0 +1,9 @@
+numeroUno = 5
+numeroDos = 10
+sumatoria = numeroUno + numeroDos
+print(f"La suma es {sumatoria}")
+
+def sumarNumeros(numeroUno, numeroDos):
+	sumatoria = numeroUno + numeroDos
+	return sumatoria
+
